54347fac1f80920681b3a3faa24bf1673091452b
Pedro Lucas <pdrolucas.dev@gmail.com>
2022-06-14 09:30:18 -0300

    feat: add jwt auth

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -2,6 +2,7 @@
 
 const repository = require('../repositories/customer.repository');
 const md5 = require('md5');
+const authService = require('../services/auth.service');
 const { StatusCodes } = require('http-status-codes');
 
 exports.post = async (req, res, next) => {
@@ -16,6 +17,41 @@ exports.post = async (req, res, next) => {
     res
       .status(StatusCodes.CREATED)
       .json({ status: 'ok', message: 'Customer criado com sucesso', response });
+  } catch (err) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      status: 'error',
+      message: 'Falha ao cadastrar customer',
+      error: err.message,
+    });
+  }
+};
+
+exports.authenticate = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const customer = await repository.authenticate({
+      email,
+      password: md5(password + process.env.SALT_KEY),
+    });
+
+    if (!customer) {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ status: 'error', message: 'Invalid email or password' });
+      return;
+    }
+
+    const token = await authService.generateToken({
+      email: customer.email,
+      name: customer.name,
+    });
+
+    res.status(StatusCodes.CREATED).json({
+      status: 'ok',
+      token,
+      data: { email: customer.email, name: customer.name },
+    });
   } catch (err) {
     res.status(StatusCodes.BAD_REQUEST).json({
       status: 'error',
--- src/repositories/customer.repository.js
+++ src/repositories/customer.repository.js
@@ -7,3 +7,11 @@ exports.create = (data) => {
   const customer = new Customer(data);
   return customer.save();
 };
+
+exports.authenticate = async ({ email, password }) => {
+  const res = await Customer.findOne({
+    email,
+    password,
+  });
+  return res;
+};
