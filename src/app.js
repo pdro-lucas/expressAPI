7649ece0ccb98816e7a97418d2e76c82beb66366
Pedro Lucas <pdrolucas.dev@gmail.com>
2022-06-14 23:17:41 -0300

    feat: enable cors

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 require('dotenv-safe').config();
 
 const express = require('express');
@@ -18,8 +19,19 @@ const productsRoutes = require('./routes/products.routes');
 const customerRoutes = require('./routes/customer.routes');
 const orderRoutes = require('./routes/order.routes');
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
+
+// enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allor-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, x-access-token'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
 // set routes
 app.use('/api/v1', appRoutes);
